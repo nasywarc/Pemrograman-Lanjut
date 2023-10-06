e28346aeab919231440ba4e29f165670d3044bad
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-06 18:14:51 +0700

    adding comments

--- lab/tugas program 6/program2.py
+++ lab/tugas program 6/program2.py
@@ -1,8 +1,10 @@
+# membuat fungsi yang mereverse sebuah tuple
 def Reverse(tuples):
     new_tup = tuples[::-1]
     return new_tup
 
 
 # Driver Code
+# membuat sebuah tuple lalu memanggil fungsi Reverse dan menampilkannya
 tuples = ('z', 'a', 'd', 'f', 'g', 'e', 'e', 'k')
 print(Reverse(tuples))
