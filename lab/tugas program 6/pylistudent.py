605aefe76e57f8554120c422b90ef51aef06df3e
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-06 22:05:20 +0700

    modify codes

--- lab/tugas program 6/pylistudent.py
+++ lab/tugas program 6/pylistudent.py
@@ -12,7 +12,8 @@ def input_data () :
     dict_data['nim'].append(input_nim)
     dict_data['major'].append(input_major)
 
-print("Welcome to Pylistudent")
+print("Welcome to Pylistudent.")
+print("This program is created to list student data.\n")
 
 dict_data = {"name" : [], "nim" : [], "major" : []}
 i = 1
@@ -20,7 +21,7 @@ loop = True
 
 while loop :
     input_data()
-    keep_loop = input("Do you wanna add another data? (Type \"Yes\" or \"No\")\nYour input : ")
+    keep_loop = input("Do you wanna add another data? (Type \"Yes\" or \"No\")\nInput : ")
     if keep_loop == 'No' or keep_loop == 'no' :
         loop = False
     elif keep_loop == 'Yes' or keep_loop == 'yes' :
@@ -29,12 +30,10 @@ while loop :
     else :
         print("Your input is invalid.")
         loop = False
-
+print("\n")
 for total in range (0, i) : 
     print(f"Student {total+1}")
-    for name in dict_data['name'] :
-        print(name)
-        for nim in dict_data['nim'] :
-            print(nim)
-            for major in dict_data['major'] :
-                print(major)
+    print(f"Name\t: {dict_data['name'][total]}")
+    print(f"NIM\t: {dict_data['nim'][total]}")
+    print(f"Major\t: {dict_data['major'][total]}")
+    
