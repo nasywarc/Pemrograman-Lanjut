2d85e8f7a39ed3b4bee21441205b0e8362da809e
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-06 19:33:59 +0700

    adding comments program 3

--- lab/tugas program 6/program3.py
+++ lab/tugas program 6/program3.py
@@ -1,19 +1,19 @@
+# mengimport Counter dari module collections
 from collections import Counter
 
-
+# fungsi untuk menghilangkan kata yang muncul lebih dari 1x
 def remov_duplicates(input):
 
-    input = input.split(" ")
+    input = input.split(" ") # memisahkan var input dg spasi sbg separatornya
 
-    for i in range(0, len(input)):
-        input[i] = "".join(input[i])
+    for i in range(0, len(input)): 
+        input[i] = "".join(input[i]) # loop untuk menggabungkan input kembali
+    
+    UniqW = Counter(input) # counter untuk menghitung elemen yg sama dalam var input
 
-    UniqW = Counter(input)
-
-    s = " ".join(UniqW.keys())
+    s = " ".join(UniqW.keys()) # menggabungkan setiap key/kunci dari dictionary UniqW
     print(s)
 
-
-if __name__ == "__main__":
+if __name__ == "__main__": # akan berjalan jika program ini dijalankan sbg program utama
     input = 'Python is great and Java is also great'
-    remov_duplicates(input)
+    remov_duplicates(input) # memanggil fungsi remov_duplicates
