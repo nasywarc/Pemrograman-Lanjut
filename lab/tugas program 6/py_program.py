4f97749f8d15e6ae0f65332343b12bde7f8125ef
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-06 21:36:20 +0700

    adding code to py program file

--- lab/tugas program 6/py_program.py
+++ lab/tugas program 6/py_program.py
@@ -2,3 +2,28 @@
 # Nasywa Azizah Zharifah
 # 225150307111060
 
+def input_data () :
+
+    input_name = input("Type your name : ")
+    input_nim = input("Type your NIM : ")
+    input_major = input("Type your major : ")
+
+    dict_data['name'].append(input_name)
+    dict_data['nim'].append(input_nim)
+    dict_data['major'].append(input_major)
+
+dict_data = {"name" : [], "nim" : [], "major" : []}
+loop = True
+
+while loop :
+    input_data()
+    keep_loop = input("Do you wanna add another data?\n(Type \"Yes\" or \"No\")\n")
+    if keep_loop == 'No' or keep_loop == 'no' :
+        loop = False
+    elif keep_loop == 'Yes' or keep_loop == 'yes' :
+        loop = True
+    else :
+        print("Your input is invalid.")
+        loop = False
+
+print(dict_data)
