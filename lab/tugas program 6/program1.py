7a145038b70f63bbaad35a6e15edf6c3281302f4
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-06 10:53:17 +0700

    adding comments

--- lab/tugas program 6/program1.py
+++ lab/tugas program 6/program1.py
@@ -1,8 +1,10 @@
 # Swap function
+# membuat fungsi swapList untuk menukar index pertama & terakhir
 def swapList(newList):
     size = len(newList)
 
     # Swapping
+    # kode dimana isi index pertama & terakhir ditukar
     temp = newList[0]
     newList[0] = newList[size - 1]
     newList[size - 1] = temp
@@ -11,6 +13,7 @@ def swapList(newList):
 
 
 # Driver code
+# membuat list newList dan mengisinya dengan angka
 newList = [12, 35, 9, 56, 24]
 
 print(swapList(newList))
