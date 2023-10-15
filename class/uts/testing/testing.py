7523c7f2292fff9d217d4e7e857839225f820bf1
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-15 10:55:45 +0700

    adding index

--- class/uts/testing/testing.py
+++ class/uts/testing/testing.py
@@ -5,15 +5,18 @@ def search_by_name (search) :
     search_lower = search.lower()
     search_capital = search.capitalize()
     search_title = search.title()
+    i = 1
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
         read_file = csv.DictReader(new_york)
         print('\n--------------------------------------------------------------')
-        print("\nResult :")
+        print("\nResult :\n")
         global found
         for row in read_file :
             if search in row['name'] or search_lower in row['name'] or search_capital in row['name'] or search_title in row['name']:
                 found = True
+                print(f"Data - {i}")
                 print(f"ID = {row['id']}\nName = {row['name']}\nHost Name =  {row['host_name']}\nNeightbourhood Group = {row['neighbourhood_group']}\nNeighbourhood = {row['neighbourhood']}\nLatitude = {row['latitude']}\nLongtitude = {row['longitude']}\nRoom Type = {row['room_type']}\nPrice = {row['price']}\nMinimum Nights = {row['minimum_nights']}\nNumber of Reviews = {row['number_of_reviews']}\nLast Review = {row['last_review']}\nReviews per Month = {row['reviews_per_month']}\nCalculated Host Listing Count = {row['calculated_host_listings_count']}\nAvailability = {row['availability_365']}")
+                i += 1
         if found == False :
             print(f"There is no name such \"{search}\"")
         print('==============================================================')
@@ -22,7 +25,7 @@ def search_by_id (search) :
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
         read_file = csv.DictReader(new_york)
         print('\n--------------------------------------------------------------')
-        print("\nResult :")
+        print("\nResult :\n")
         global found
         for row in read_file :
             if search == row['id']:
