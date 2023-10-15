e8e9b3d84d16a8b0f5d64131c7305c3d7294a684
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-15 12:11:05 +0700

    deleting comment, reducing code

--- class/uts/testing/testing.py
+++ class/uts/testing/testing.py
@@ -3,17 +3,13 @@ import csv
 import art
 
 def search_by_name (search) :
-    search_lower = search.lower()
-    search_capital = search.capitalize()
-    search_title = search.title()
     i = 1
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
         read_file = csv.DictReader(new_york)
-        #print('\n--------------------------------------------------------------')
         print("\nResult :\n")
         global found
         for row in read_file :
-            if search in row['name'] or search_lower in row['name'] or search_capital in row['name'] or search_title in row['name']:
+            if search.lower() in row['name'].lower():
                 found = True
                 print(f"Data - {i}")
                 print(f"\tID = {row['id']}\n\tName = {row['name']}\n\tHost Name =  {row['host_name']}\n\tNeightbourhood Group = {row['neighbourhood_group']}\n\tNeighbourhood = {row['neighbourhood']}\n\tLatitude = {row['latitude']}\n\tLongtitude = {row['longitude']}\n\tRoom Type = {row['room_type']}\n\tPrice = {row['price']}\n\tMinimum Nights = {row['minimum_nights']}\n\tNumber of Reviews = {row['number_of_reviews']}\n\tLast Review = {row['last_review']}\n\tReviews per Month = {row['reviews_per_month']}\n\tCalculated Host Listing Count = {row['calculated_host_listings_count']}\n\tAvailability = {row['availability_365']}\n")
@@ -24,7 +20,6 @@ def search_by_name (search) :
 def search_by_id (search) :
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
         read_file = csv.DictReader(new_york)
-        #print('\n--------------------------------------------------------------')
         print("\nResult :\n")
         global found
         for row in read_file :
@@ -36,7 +31,6 @@ def search_by_id (search) :
             print(f"There is no ID such \"{search}\"\n")
 
 os.system('cls')
-#print('============================================================')
 print(art.logo)
 found = False
 loop = True
@@ -44,7 +38,6 @@ run_again = 0
 while loop :
     if run_again > 0 :
         os.system('cls')
-        #print('============================================================')
         print(art.logo)
     print('============================================================')
     search_by = input("1. Search by ID\n2. Search by Name\n3. Exit\nInput (1 / 2 / 3) -> ")
