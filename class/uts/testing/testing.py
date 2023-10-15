8b7968a105d4a3c551ae192da25df618d4ff5518
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-15 09:47:28 +0700

    adding codes

--- class/uts/testing/testing.py
+++ class/uts/testing/testing.py
@@ -10,26 +10,34 @@ def search_by_name (search) :
         print("\nResult :")
         for row in read_file :
             if search_lower in row['name']:
+                found = True
                 print(f"{row['name']}")
             elif search_capital in row['name'] :
+                found = True
                 print(f"{row['name']}")
             elif search_title in row['name']:
+                found = True
                 print(f"{row['name']}")
-            else:
-                print(f"There is no name such \"{search}\"")
+        if found == False :
+            print(f"There is no name such \"{search}\"")
 
 def search_by_id (search) :
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
         read_file = csv.DictReader(new_york)
         print("\nResult :")
         for row in read_file :
-            if search in row['id']:
+            if search == row['id']:
                 print(f"{row['id']}\t{row['name']}")
 
 
 os.system('cls')
-search = input("Search : \n")
-search_by_id(search)
+found = False
+search_by = input("Search by (\'ID\' or \'Name\'):\nInput --> ")
+search = input("Search : \nInput --> ")
+if search_by == "ID" or search_by == "id" :
+    search_by_id(search)
+elif search_by == "Name" or search_by == "name" :
+    search_by_name(search)
 
 # with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
 #         read_file = csv.DictReader(new_york)
