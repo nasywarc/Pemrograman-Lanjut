bb72ee1945b18bf0d9f4be145ec792ead627fbcd
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-15 11:39:28 +0700

    endline

--- class/uts/testing/testing.py
+++ class/uts/testing/testing.py
@@ -18,7 +18,7 @@ def search_by_name (search) :
                 print(f"\tID = {row['id']}\n\tName = {row['name']}\n\tHost Name =  {row['host_name']}\n\tNeightbourhood Group = {row['neighbourhood_group']}\n\tNeighbourhood = {row['neighbourhood']}\n\tLatitude = {row['latitude']}\n\tLongtitude = {row['longitude']}\n\tRoom Type = {row['room_type']}\n\tPrice = {row['price']}\n\tMinimum Nights = {row['minimum_nights']}\n\tNumber of Reviews = {row['number_of_reviews']}\n\tLast Review = {row['last_review']}\n\tReviews per Month = {row['reviews_per_month']}\n\tCalculated Host Listing Count = {row['calculated_host_listings_count']}\n\tAvailability = {row['availability_365']}\n")
                 i += 1
         if found == False :
-            print(f"There is no name such \"{search}\"")
+            print(f"There is no name such \"{search}\"\n")
 
 def search_by_id (search) :
     with open("C:/Users/Nasywa Azizah/data/coding/git remote/Pemrograman-Lanjut/class/uts/testing/csv/new_york_housing.csv", "r", newline='', encoding="cp437", errors='ignore') as new_york:
@@ -32,7 +32,7 @@ def search_by_id (search) :
                 print("Data")
                 print(f"ID = {row['id']}\nName = {row['name']}\nHost Name =  {row['host_name']}\nNeightbourhood Group = {row['neighbourhood_group']}\nNeighbourhood = {row['neighbourhood']}\nLatitude = {row['latitude']}\nLongtitude = {row['longitude']}\nRoom Type = {row['room_type']}\nPrice = {row['price']}\nMinimum Nights = {row['minimum_nights']}\nNumber of Reviews = {row['number_of_reviews']}\nLast Review = {row['last_review']}\nReviews per Month = {row['reviews_per_month']}\nCalculated Host Listing Count = {row['calculated_host_listings_count']}\nAvailability = {row['availability_365']}\n")
         if found == False :
-            print(f"There is no ID such \"{search}\"")
+            print(f"There is no ID such \"{search}\"\n")
 
 os.system('cls')
 print('''
