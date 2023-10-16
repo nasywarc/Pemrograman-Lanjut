e0cb03c972b7ab9f7b1de39b51f4eda31c3e8d00
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-16 15:13:31 +0700

    editing code

--- class/uts/testing/uts_pemlan.py
+++ class/uts/testing/uts_pemlan.py
@@ -243,7 +243,7 @@ while loop :
         print('============================================================')
         # print(art.logo)
     print('============================================================')
-    search_by = input("1. Show data\n2. Find data by ID\n3. Find data by Name\n4. Find by Filter\n5. Add data\n6. Update data availability\n7. Delete data\n8. Help\n9. Exit\nInput (1-7) -> ")
+    search_by = input("1. Show data\n2. Find data by ID\n3. Find data by Name\n4. Find by Filter\n5. Add data\n6. Update data availability\n7. Delete data\n8. Help\n9. Exit\nInput (1-9) -> ")
 
     if search_by == '1' :
         show()
