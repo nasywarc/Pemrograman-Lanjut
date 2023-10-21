1947957c6f71f0fc524abf88d53d77f1b6e8a4ba
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-21 17:24:18 +0700

    adding comments to palindrom and maze file

--- lab/tugas program 7/modul/BanyakJalanMenujuPintuKeluar.py
+++ lab/tugas program 7/modul/BanyakJalanMenujuPintuKeluar.py
@@ -1,5 +1,10 @@
+# Nasywa Azizah Zharifah
+# 225150307111060
+
+# menginisialisasi posisi awal pada (1, 1)
 x = y = 1
 
+# membuat fungsi untuk menghitung banyak cara keluar dari maze
 def maze(length, width, x, y):
     if x == length and y == width:
         return 1
@@ -7,6 +12,9 @@ def maze(length, width, x, y):
         return 0
     return maze(length, width, x + 1, y) + maze(length, width, x, y + 1)
 
+# menyimpan variable tinggi dan lebar dalam bentuk integer
 tinggi = int(input("Tinggi maze: "))
 lebar = int(input("Lebar maze: "))
+
+# memanggil fungsi maze sekaligus menampilkan hasilnya ke user
 print(maze(tinggi, lebar, x, y))
--- lab/tugas program 7/modul/Palindrom.py
+++ lab/tugas program 7/modul/Palindrom.py
@@ -1,17 +1,25 @@
+# Nasywa Azizah Zharifah
+# 225150307111060
+
+# membuat fungsi untuk mengecek persamaan
 def check(original, edited):
     if original == edited :
         print('Palindrom')
     else :
         print('Bukan palindrom')
 
+# membuat fungsi untuk membalikkan kata
 def reversed(word):
     return word[::-1]
 
+# membuat list untuk menampung var kata
 kata = []
 n = int(input("Jumlah huruf yang ingin diperiksa : "))
 
+# looping untuk menyimpan nilai list kata
 for i in range(n):
     kata.append(input("Tuliskan sebuah kata : "))
 
+# looping untuk memanggil fungsi check sekaligus reversed
 for i in range(n) :
     check(kata[i], reversed(kata[i]))
