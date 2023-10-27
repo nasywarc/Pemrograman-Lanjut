93edb2590b6f9f46538e72d618d69c5e14837843
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 16:38:47 +0700

    using program_1_modif.py as a reference

--- lab/tugas program 8/pyclass.py
+++ lab/tugas program 8/pyclass.py
@@ -2,6 +2,8 @@
 # Nasywa Azizah Zharifah
 # 225150307111060
 
+import os
+
 class Tugas:
 
     total = 0
@@ -12,6 +14,8 @@ class Tugas:
         self.tgl = deadline
         self.status = apakah_selesai
 
+        Tugas.total += 1
+
     def __del__(self):
         Tugas.total -= 1
 
@@ -19,18 +23,20 @@ class Tugas:
         print(f'Mata Kuliah\t: {self.mk}')
         print(f'Deskripsi\t: {self.deskripsi}')
         print(f'Batas Akhir\t: {self.tgl}')
-        print(f'Status\t: {self.status}')
+        print(f'Status\t\t: {self.status}')
 
     def ubah_status(self):
         pass
 
     def total_tugas(cls):
-        print(f'Jumlah tugas\t: {cls.total}')
+        print(f'\nJumlah tugas\t: {cls.total}')
 
     total_tugas = classmethod(total_tugas)
 
 daftar_tugas = []
 loop = True
+
+os.system('cls')
 
 while loop:
     
@@ -40,7 +46,7 @@ while loop:
     deskripsi_tugas = input('Masukkan deskripsi : ')
     deadline_tugas = input('Masukkan batas pengumpulan : ')
     status_tugas = 'belum selesai'
-    daftar_tugas.append(Tugas(matakuliah, deskripsi_tugas, status_tugas))
+    daftar_tugas.append(Tugas(matakuliah, deskripsi_tugas, deadline_tugas, status_tugas))
     
     # prompt apakah ingin menambahkan lagi
     menambah_orang = input('\nApa Anda ingin menambah orang lagi? "Ya" atau "Tidak".\nInput -> ').lower()
@@ -53,14 +59,15 @@ while loop:
     
     print('')
 
+print('--------------')
+print('| LIST TUGAS |')
+print('--------------')
+
 # menampilkan semua object untuk mengatakan halo
-for semua_orang in orang :
-    semua_orang.katakanHalo()
+for semua_tugas in daftar_tugas :
+    semua_tugas.print_tugas()
 
 # menampilkan total populasi
-Orang.total_populasi()
+Tugas.total_tugas()
 
 
-        # print('----------')
-        # print('LIST TUGAS')
-        # print('----------')
