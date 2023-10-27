38c605797d52e69074f187ce555ee89c1861d920
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 21:12:13 +0700

    make the program loop

--- lab/tugas program 8/pyclass.py
+++ lab/tugas program 8/pyclass.py
@@ -34,76 +34,99 @@ class Tugas:
     total_tugas = classmethod(total_tugas)
 
 daftar_tugas = []
-loop = True
+program = True
 
-os.system('cls')
+while program:
 
-while loop:
-    
-    # prompt untuk user mengisi data
-    # akan dimasukkan ke list orang dalam bentuk Tugas
-    matakuliah = input('Masukkan Mata Kuliah\t\t: ')
-    materi_tugas = input('Masukkan Materi Tugas\t\t: ')
-    deadline_tugas = input('Masukkan Batas Pengumpulan\t: ')
-    status_tugas = 'Belum Dikerjakan'
-    daftar_tugas.append(Tugas(matakuliah, materi_tugas, deadline_tugas, status_tugas))
-    
-    # prompt apakah ingin menambahkan lagi
-    menambah_tugas = input('\nApa Anda ingin daftar tugas lagi? "Ya" atau "Tidak".\nInput -> ').lower()
 
-    if menambah_tugas == 'tidak':
-        loop = False
-    elif menambah_tugas != 'ya':
-        print('Input anda tidak valid.')
-        loop = False
-    
-    print('')
+    os.system('cls')
+    print('Selamat datang di program Pyclass. Silakan pilih salah satu menu di bawah :')
+    print('1. Menambahkan tugas ke daftar.')
+    print('2. Menampilkan tugas di daftar.')
+    print('3. Menghapus tugas dari daftar.')
+    print('4. Menandai penyelesaian tugas.')
+    print('5. Keluar program.')
 
-os.system('cls')
-print('--------------')
-print('| LIST TUGAS |')
-print('--------------')
+    pilihan = int(input('Input -> '))
 
-# menampilkan semua object tugas untuk ditampilkan
-for index in range(len(daftar_tugas)) :
-    print(f'Tugas ke-{index+1}')
-    daftar_tugas[index].print_tugas()
-    print('')
+    if pilihan == 1:
+        loop = True
+        while loop:
+            
+            # prompt untuk user mengisi data
+            # akan dimasukkan ke list orang dalam bentuk Tugas
+            matakuliah = input('Masukkan Mata Kuliah\t\t: ')
+            materi_tugas = input('Masukkan Materi Tugas\t\t: ')
+            deadline_tugas = input('Masukkan Batas Pengumpulan\t: ')
+            status_tugas = 'Belum Dikerjakan'
+            daftar_tugas.append(Tugas(matakuliah, materi_tugas, deadline_tugas, status_tugas))
+            
+            # prompt apakah ingin menambahkan lagi
+            menambah_tugas = input('\nApa Anda ingin daftar tugas lagi? "Ya" atau "Tidak".\nInput -> ').lower()
 
-# menampilkan total tugas
-Tugas.total_tugas()
-
-loop = True
-
-while loop :
-    # prompt apa ingin menghapus tugas
-    menghapus_tugas = input('\nApa Anda ingin menghapus tugas? "Ya" atau "Tidak".\nInput -> ').lower()
-    
-    # jika iya, akan menghapus tugas yang berada di akhir list
-    if menghapus_tugas == 'ya':
-        if daftar_tugas :
-            del daftar_tugas[-1]
-
+            if menambah_tugas == 'tidak':
+                loop = False
+            elif menambah_tugas != 'ya':
+                print('Input anda tidak valid.')
+                loop = False
+            
             print('')
+
+    elif pilihan == 2:
+        os.system('cls')
+        print('--------------')
+        print('| LIST TUGAS |')
+        print('--------------')
+
+        # menampilkan semua object tugas untuk ditampilkan
+        for index in range(len(daftar_tugas)) :
+            print(f'Tugas ke-{index+1}')
+            daftar_tugas[index].print_tugas()
+            print('')
+
+        # menampilkan total tugas
+        Tugas.total_tugas()
+        lanjut = input('\tLanjutkan program? "Ya" atau "Tidak".\nInput -> ').lower()
+        if lanjut != 'ya':
+            program = False
+
+    elif pilihan == 3:
+        loop = True
+        while loop :
+            # prompt apa ingin menghapus tugas
+            menghapus_tugas = input('\nApa Anda ingin menghapus tugas? "Ya" atau "Tidak".\nInput -> ').lower()
             
-            # menampilkan informasi untuk semua object yang tersisa
-            for index in range(len(daftar_tugas)) :
-                print(f'Tugas ke-{index+1}')
-                daftar_tugas[index].print_tugas()
+            # jika iya, akan menghapus tugas yang berada di akhir list
+            if menghapus_tugas == 'ya':
+                if daftar_tugas :
+                    del daftar_tugas[-1]
+
+                    print('')
+                    
+                    # menampilkan informasi untuk semua object yang tersisa
+                    for index in range(len(daftar_tugas)) :
+                        print(f'Tugas ke-{index+1}')
+                        daftar_tugas[index].print_tugas()
+                        print('')
+                    
+                    # jika len(orang) sudah 0, akan menghapus paksa object
+                    if len(daftar_tugas) == 0 :
+                        Tugas.__del__(daftar_tugas)
+                        print('Semua tugas sudah terhapus.')
+                        loop = False
+
+            elif menghapus_tugas == 'tidak' :
                 print('')
-            
-            # jika len(orang) sudah 0, akan menghapus paksa object
-            if len(daftar_tugas) == 0 :
-                Tugas.__del__(daftar_tugas)
-                print('Semua tugas sudah terhapus.')
+                loop = False
+            else :
+                print('\nInput anda tidak valid.')
                 loop = False
 
-    elif menghapus_tugas == 'tidak' :
-        print('')
-        loop = False
-    else :
-        print('\nInput anda tidak valid.')
-        loop = False
+    elif pilihan == 5:
+        program = False
+
+    else:
+        print('Input Anda tidak valid.')
 
     # menampilkan total tugas yang tersisa
-    Tugas.total_tugas()
+    # Tugas.total_tugas()
