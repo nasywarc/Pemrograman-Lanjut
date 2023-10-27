7e4a8bb9351bfa2165f040cbbddfc59c41a8f5f6
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 16:11:35 +0700

    creating the class and it method

---
+++ lab/tugas program 8/pyclass.py
@@ -0,0 +1,21 @@
+# PROGRAM PYCLASS
+# Nasywa Azizah Zharifah
+# 225150307111060
+
+class Tugas:
+
+    total = 0
+
+    def __init__(self) -> None:
+        pass
+
+    def __del__(self):
+        pass
+
+    def print_tugas(self):
+        pass
+
+    def total_tugas(self):
+        pass
+
+    total_tugas = classmethod(total_tugas)
