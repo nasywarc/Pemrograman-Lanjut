1a02cceb9c4f1dc1935d2b30e2fb2a3a9d0ed3a1
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 16:22:49 +0700

    filling the method code

--- lab/tugas program 8/pyclass.py
+++ lab/tugas program 8/pyclass.py
@@ -6,16 +6,27 @@ class Tugas:
 
     total = 0
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, nama_matkul, deskripsi, deadline, apakah_selesai):
+        self.mk = nama_matkul
+        self.deskripsi = deskripsi
+        self.tgl = deadline
+        self.status = apakah_selesai
 
     def __del__(self):
-        pass
+        Tugas.total -= 1
 
     def print_tugas(self):
-        pass
+        print(f'Mata Kuliah\t: {self.mk}')
+        print(f'Deskripsi\t: {self.deskripsi}')
+        print(f'Batas Akhir\t: {self.tgl}')
+        print(f'Status\t: {self.status}')
 
-    def total_tugas(self):
-        pass
+    def total_tugas(cls):
+        print(f'Jumlah tugas\t: {cls.total}')
 
     total_tugas = classmethod(total_tugas)
+
+    
+        # print('----------')
+        # print('LIST TUGAS')
+        # print('----------')
