4b2b5363b1641bd308d8f9e50b7238b1b5adee94
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-27 16:28:55 +0700

    editing the code

--- lab/tugas program 8/pyclass.py
+++ lab/tugas program 8/pyclass.py
@@ -21,12 +21,46 @@ class Tugas:
         print(f'Batas Akhir\t: {self.tgl}')
         print(f'Status\t: {self.status}')
 
+    def ubah_status(self):
+        pass
+
     def total_tugas(cls):
         print(f'Jumlah tugas\t: {cls.total}')
 
     total_tugas = classmethod(total_tugas)
 
+daftar_tugas = []
+loop = True
+
+while loop:
     
+    # prompt untuk user mengisi data
+    # akan dimasukkan ke list orang dalam bentuk Orang
+    matakuliah = input('Masukkan mata kuliah : ')
+    deskripsi_tugas = input('Masukkan deskripsi : ')
+    deadline_tugas = input('Masukkan batas pengumpulan : ')
+    status_tugas = 'belum selesai'
+    daftar_tugas.append(Tugas(matakuliah, deskripsi_tugas, status_tugas))
+    
+    # prompt apakah ingin menambahkan lagi
+    menambah_orang = input('\nApa Anda ingin menambah orang lagi? "Ya" atau "Tidak".\nInput -> ').lower()
+
+    if menambah_orang == 'tidak':
+        loop = False
+    elif menambah_orang != 'ya':
+        print('Input anda tidak valid.')
+        loop = False
+    
+    print('')
+
+# menampilkan semua object untuk mengatakan halo
+for semua_orang in orang :
+    semua_orang.katakanHalo()
+
+# menampilkan total populasi
+Orang.total_populasi()
+
+
         # print('----------')
         # print('LIST TUGAS')
         # print('----------')
