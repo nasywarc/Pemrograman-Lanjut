cd55a922fa3283ede4b55a4915823ab501b98978
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-31 07:46:40 +0700

    copying program_2 code

---
+++ lab/tugas program 9/modul/program_2_comment.py
@@ -0,0 +1,10 @@
+# x = "Hi!"     #commented
+
+try:
+    print(x)
+except NameError:
+    print("Variable x is not defined")
+except TypeError:
+    print("Something else went wrong")
+else:
+    print("Nothing went wrong")
