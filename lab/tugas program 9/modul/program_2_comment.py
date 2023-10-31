65ff29c80b249dffe54427e0083b83672ded0cda
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-10-31 07:49:32 +0700

    adding comments

--- lab/tugas program 9/modul/program_2_comment.py
+++ lab/tugas program 9/modul/program_2_comment.py
@@ -1,10 +1,16 @@
+# penginisialan variable x yang sebelumnya dicomment
 # x = "Hi!"     #commented
 
+# menggunakan perintah try untuk mencoba mengeksekusi kode dalam blok kode try
 try:
+    # menampilkan x
     print(x)
+# menangkap NameError jika terjadi error    
 except NameError:
     print("Variable x is not defined")
+# menangkap TypeError jika terjadi error
 except TypeError:
     print("Something else went wrong")
+# kondisi jika tidak terjadi error
 else:
     print("Nothing went wrong")
