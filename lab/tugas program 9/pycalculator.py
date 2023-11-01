d4676c6e3676e1fde8ab0f553fef7aee444b8f09
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-01 21:06:36 +0700

    making func

--- lab/tugas program 9/pycalculator.py
+++ lab/tugas program 9/pycalculator.py
@@ -5,3 +5,25 @@ class Calculator:
     def __init__(self, first_number, second_number):
         self.a = first_number
         self.b = second_number
+
+    def calculate(self):
+        pass
+
+class Sum(Calculator):
+    def calculate(self):
+        return self.a + self.b
+    
+class Sub(Calculator):
+    def calculate(self):
+        return self.a - self.b
+    
+class Mul(Calculator):
+    def calculate(self):
+        return self.a * self.b
+    
+class Div(Calculator):
+    def calculate(self):
+        return self.a / self.b
+    
+angka_1 = input('Masukkan angka pertama : ')
+angka_2 = input('Masukkan angka kedua : ')
