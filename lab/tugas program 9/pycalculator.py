4c52e72f8f07216151e7460e6ec7fc19329a4239
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-01 21:29:34 +0700

    finishing program for today

--- lab/tugas program 9/pycalculator.py
+++ lab/tugas program 9/pycalculator.py
@@ -36,15 +36,26 @@ print(('Selamat datang di program Pycalculator.'))
 pilihan_user = input('Silakan pilih operasi yang ingin Anda gunakan.\n1. Pertambahan\
                      \n2. Pengurangan\n3. Perkalian\n4. Pembagian\nInput -> ')
 
-angka_1 = int(input('Masukkan angka pertama : '))
+if pilihan_user not in ['1', '2', '3', '4']:
+    print('\nInput Anda invalid.')
+    exit()
+
+angka_1 = int(input('\nMasukkan angka pertama : '))
 angka_2 = int(input('Masukkan angka kedua : '))
 
-sum_result = Tambah(angka_1, angka_2)
-sub_result = Kurang(angka_1, angka_2)
-mul_result = Kali(angka_1, angka_2)
-div_result = Bagi(angka_1, angka_2)
+if pilihan_user == '1':
+    sum_result = Tambah(angka_1, angka_2)
+    print(f'\nHasil penjumlahan dari {angka_1} + {angka_2} adalah {sum_result.calculate()}')
+elif pilihan_user == '2':
+    sub_result = Kurang(angka_1, angka_2)
+    print(f'\nHasil pengurangan dari {angka_1} - {angka_2} adalah {sub_result.calculate()}')
+elif pilihan_user == '3':
+    mul_result = Kali(angka_1, angka_2)
+    print(f'\nHasil perkalian dari {angka_1} * {angka_2} adalah {mul_result.calculate()}')
+elif pilihan_user == '4':
+    div_result = Bagi(angka_1, angka_2)
+    print(f'\nHasil pembagian dari {angka_1} / {angka_2} adalah {div_result.calculate()}')
+else :
+    print('\nInput Anda invalid.')
 
-print(f'\nHasil penjumlahan dari {angka_1} + {angka_2} adalah {sum_result.calculate()}')
-print(f'Hasil pengurangan dari {angka_1} - {angka_2} adalah {sub_result.calculate()}')
-print(f'Hasil perkalian dari {angka_1} * {angka_2} adalah {mul_result.calculate()}')
-print(f'Hasil pembagian dari {angka_1} / {angka_2} adalah {div_result.calculate()}')
+
