3443c3b8ed64782d5ff9597b2be8b9a772a16368
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-02 16:59:43 +0700

    menu, menu button, radio button, entry text example code

--- class/08_GUI/program_1.py
+++ class/08_GUI/program_1.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import *
 
 tk = tkinter.Tk()
 # tk.title('New Window')
@@ -14,12 +15,30 @@ tk = tkinter.Tk()
 # button.pack()
 # tk.mainloop()
 
-tk.title('CheckButton')
-CheckVar1 = tkinter.IntVar()
-CheckVar2 = tkinter.IntVar()
-C1 = tkinter.Checkbutton(tk, text='Laki - laki', variable=CheckVar1, onvalue=1, offvalue=0, height=5, width=20)
-C2 = tkinter.Checkbutton(tk, text='Perempuan', variable=CheckVar2, onvalue=1, offvalue=0, height=5, width=20)
+# tk.title('CheckButton')
+# CheckVar1 = tkinter.IntVar()
+# CheckVar2 = tkinter.IntVar()
+# C1 = tkinter.Checkbutton(tk, text='Laki - laki', variable=CheckVar1, onvalue=1, offvalue=0, height=5, width=20)
+# C2 = tkinter.Checkbutton(tk, text='Perempuan', variable=CheckVar2, onvalue=1, offvalue=0, height=5, width=20)
 
-C1.pack()
-C2.pack()
-tk.mainloop()
+# C1.pack()
+# C2.pack()
+# tk.mainloop()
+
+# drop down menu
+tk.title('Menu')
+def donothing():
+    filewin = tkinter.Toplevel(tk)
+
+
+tk.title('Menu Button')
+
+
+# memilih button
+tk.title('Radio Button')
+
+# user dapat menggunakan keyboard
+tk.title('Entry Text')
+L1 = Label(tk, text='User Name')
+L1.pack(side=LEFT)
+E1 = Entry(tk)
