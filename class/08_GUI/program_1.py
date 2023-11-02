8b419e36b7ce33daef0dde4f355f849089bae123
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-02 16:25:45 +0700

    canvas example code

--- class/08_GUI/program_1.py
+++ class/08_GUI/program_1.py
@@ -1,5 +1,10 @@
 import tkinter
 
 tk = tkinter.Tk()
-tk.title('New Window')
+# tk.title('New Window')
+# tk.mainloop()
+
+tk.title('Canvas')
+canvas = tkinter.Canvas(tk, width=300, height=300)
+canvas.grid()
 tk.mainloop()
