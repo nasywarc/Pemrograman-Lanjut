591b9ddce060fa5f83c9cb59a1834f5b6511e3cf
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-05 06:24:47 +0700

    file for practicing classes

---
+++ lab/tugas program 9/code_with_mosh.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print('talk')
+
+    def say_hi(self):
+        print(f'Hi, my name is {self.name}.')
+
+person1 = Person('Nasywa')
+person1.talk()
+person1.say_hi()
