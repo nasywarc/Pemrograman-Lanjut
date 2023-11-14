9c0b568ac790c480c63ad1225c00faa178e80cfe
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-14 23:51:26 +0700

    grid example

--- lab/tugas program 10/gui_tutorial.py
+++ lab/tugas program 10/gui_tutorial.py
@@ -1,10 +1,29 @@
+# # hello world program
+
+# from tkinter import *
+
+# root = Tk()
+
+# # creatine a label widget
+# myLabel = Label(root, text='Hello World!')
+# # showing it onto the screen
+# myLabel.pack()
+
+# root.mainloop()
+
+
+# grid program
+
 from tkinter import *
 
 root = Tk()
 
 # creatine a label widget
-myLabel = Label(root, text='Hello World!')
+myLabel1 = Label(root, text='Hello World!')
+myLabel2 = Label(root, text='My Name Is John Elder!')
 # showing it onto the screen
-myLabel.pack()
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
 
 root.mainloop()
