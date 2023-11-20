0f98e3e6e7f8b54d5fa963bbe920fad90c460635
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-20 23:57:24 +0700

    new label

--- lab/tugas program 10/my_program.py
+++ lab/tugas program 10/my_program.py
@@ -1,8 +1,15 @@
 from tkinter import *
 
+BLACK = '#000'
+WHITE = '#FFF'
+
 window = Tk()
 
 window.title('My Program')
 window.minsize(width=300, height=300)
+
+my_label = Label(text='Lorem Ipsum', font=(20), fg=BLACK)
+my_label.pack()
+
 
 window.mainloop()
