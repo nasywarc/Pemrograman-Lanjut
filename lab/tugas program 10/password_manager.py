33d9489be96ff4e18d567b250423ed4bd6840e00
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-24 00:44:41 +0700

    deleting the remaining code, set the data.txt file path

--- lab/tugas program 10/password_manager.py
+++ lab/tugas program 10/password_manager.py
@@ -3,16 +3,4 @@ from tkinter import *
 from tkinter import messagebox
 import pyperclip
 
-BLACK = '#00F' # 000
-WHITE = '#FF0' # FFF
-
-window = Tk()
-
-window.title('Password Manager')
-window.minsize(width=400, height=300)
-
-my_label = Label(text='This is Password Manager', font=(20), fg=BLACK)
-my_label.pack()
-
-
-window.mainloop()
+file_path = 'data.txt'
