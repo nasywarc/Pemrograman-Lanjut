edefbfe5df91afa1b53f631d3717df9ef424c103
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-23 23:57:04 +0700

    change the width

--- lab/tugas program 10/my_program.py
+++ lab/tugas program 10/my_program.py
@@ -6,7 +6,7 @@ WHITE = '#FF0' # FFF
 window = Tk()
 
 window.title('Password Manager')
-window.minsize(width=300, height=300)
+window.minsize(width=400, height=300)
 
 my_label = Label(text='Lorem Ipsum', font=(20), fg=BLACK)
 my_label.pack()
