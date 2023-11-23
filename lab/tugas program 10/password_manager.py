08734a16f86df48fd474fb12b113d3de7df98734
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-24 00:46:04 +0700

    creating the canvas section

--- lab/tugas program 10/password_manager.py
+++ lab/tugas program 10/password_manager.py
@@ -8,3 +8,8 @@ file_path = 'data.txt'
 window = Tk()
 window.title('Password Manager')
 window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
