820d965c9fb426c4e5cb51762badf535b78a19a9
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-28 21:31:40 +0700

    modify singlethreading

---
+++ lab/tugas program 11/SingleThreading_modif.py
@@ -0,0 +1,28 @@
+import threading
+from time import *
+
+def sqr(n):
+    for x in n:
+        sleep(1)
+        
+def cube(n):
+    for x in n:
+        sleep(1)
+
+        
+n = [1,2,3,4,5,6,7,8]
+
+t1 =threading.Thread(target=sqr, args=(10,))
+t2 =threading.Thread(target=cube, args=(10,))
+
+start = time()
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+end = time()
+
+print("Waktu mulai :", start)
+print("Waktu selesai :", end)
+print("Durasi eksekusi program:", end-start)
+
