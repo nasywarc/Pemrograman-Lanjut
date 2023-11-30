b4f0e22dd4c4b629fead68d070ae0fa019ba39ac
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-11-30 08:53:29 +0700

    try to make the code works

--- class/uts/dataframe implement/uts_pemlan_gui.py
+++ class/uts/dataframe implement/uts_pemlan_gui.py
@@ -1,140 +1,126 @@
-import tkinter as tk
-from tkinter import messagebox
-import socket
-import threading
+from tkinter import *
 import pandas as pd
 
-class HousingApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Housing Data Management")
-        
-        # GUI Components
-        self.label = tk.Label(master, text="Select an action:")
-        self.label.pack()
+def set_button_color(button, color, textcolor):
+    show_button.config(bg='SystemButtonFace', fg='Black')
+    search_id_button.config(bg='SystemButtonFace', fg='Black')
+    search_name_button.config(bg='SystemButtonFace', fg='Black')
+    search_filter_button.config(bg='SystemButtonFace', fg='Black')
+    add_button.config(bg='SystemButtonFace', fg='Black')
+    update_button.config(bg='SystemButtonFace', fg='Black')
+    delete_button.config(bg='SystemButtonFace', fg='Black')
+    help_button.config(bg='SystemButtonFace', fg='Black')
 
-        self.show_button = tk.Button(master, text="Show Data", command=self.show_data)
-        self.show_button.pack()
+    button.config(bg=color, fg=textcolor)
 
-        self.find_id_button = tk.Button(master, text="Find Data by ID", command=self.find_data_by_id)
-        self.find_id_button.pack()
+def show():
+    set_button_color(show_button, '#67B274', '#FFFFFF')
+    # show_data()
 
-        self.find_name_button = tk.Button(master, text="Find Data by Name", command=self.find_data_by_name)
-        self.find_name_button.pack()
+# def show_data():
+#     show_window = Toplevel(window)
+#     show_window.title('Show Data')
+#     data_listbox = Listbox(show_window, width=50, height=10)
+#     data_listbox.pack(padx=10, pady=10)
+#     data = df[['id', 'name', 'neighbourhood_group', 'price']].to_string(index=False)
+#     data_listbox.insert(END, data)
 
-        self.find_filter_button = tk.Button(master, text="Find Data by Filter", command=self.find_data_by_filter)
-        self.find_filter_button.pack()
+# def show_result():
+#     show_result_window = Toplevel(window)
+#     show_result_window.title('Result')
+#     result_label = Label(show_result_window, text="\nResult\n------")
+#     result_label.pack(padx=10, pady=10)
+#     result_text = df[['id', 'name', 'neighbourhood_group', 'price']].to_string(index=False)
+#     result_listbox = Listbox(show_result_window, width=50, height=10)
+#     result_listbox.pack(padx=10, pady=10)
+#     result_listbox.insert(END, result_text)
 
-        self.add_button = tk.Button(master, text="Add Data", command=self.add_data)
-        self.add_button.pack()
+def search_id():
+    set_button_color(search_id_button, '#67B274', '#FFFFFF')
 
-        self.update_button = tk.Button(master, text="Update Data Availability", command=self.update_data)
-        self.update_button.pack()
+def search_name():
+    set_button_color(search_name_button, '#67B274', '#FFFFFF')
 
-        self.delete_button = tk.Button(master, text="Delete Data", command=self.delete_data)
-        self.delete_button.pack()
+def search_filter():
+    set_button_color(search_filter_button, '#67B274', '#FFFFFF')
 
-        self.exit_button = tk.Button(master, text="Exit", command=self.master.destroy)
-        self.exit_button.pack()
+def add():
+    set_button_color(add_button, '#67B274', '#FFFFFF')
 
-        # Networking
-        self.server_thread = threading.Thread(target=self.start_server)
-        self.server_thread.start()
+def update():
+    set_button_color(update_button, '#67B274', '#FFFFFF')
 
-    def start_server(self):
-        # Start the server on a separate thread
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind(('localhost', 12345))  # Use any available port
-        server_socket.listen(5)
+def delete():
+    set_button_color(delete_button, '#67B274', '#FFFFFF')
 
-        while True:
-            client_socket, addr = server_socket.accept()
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_thread.start()
+def help():
+    set_button_color(help_button, '#67B274', '#FFFFFF')
 
-    def handle_client(self, client_socket):
-        # Handle communication with a client
-        try:
-            data = client_socket.recv(1024).decode()
-            if data == 'show':
-                result = self.get_data_as_string()
-                client_socket.send(result.encode())
-            elif data.startswith('find_by_id'):
-                _, search_id = data.split(',')
-                result = self.search_by_id(search_id)
-                client_socket.send(result.encode())
-            # Add similar handling for other actions (find_by_name, find_by_filter, add, update, delete)
-        except Exception as e:
-            print(f"Error handling client: {e}")
-        finally:
-            client_socket.close()
+    help_window = Toplevel(window)
+    help_window.title('Help Menu')
 
-    def show_data(self):
-        # Send request to server to get data
-        self.send_request('show')
+    help_text = "HELP MENU\n\n" \
+                "1. Show data\t\t: Show every data in CSV file.\n" \
+                "2. Find data by ID\t\t: Search data by Housing ID.\n" \
+                "3. Find data by Name\t: Search data by the name of the housing.\n" \
+                "4. Find data by Filter\t: Filter search result based on Neighbourhood Group, Neighbourhood, " \
+                "and Price.\n" \
+                "5. Add data\t\t: Add new entry to the CSV file.\n" \
+                "6. Update data\t\t: Update availability of the housing.\n" \
+                "7. Delete data\t\t: Delete entry by id.\n" \
+                "9. Exit\t\t\t: Stop the program.\n" \
+                # "================================ HELP MENU ================================\n\n" \
+                # "=========================================================================" \
 
-    def find_data_by_id(self):
-        search_id = tk.simpledialog.askstring("Find Data by ID", "Enter ID:")
-        if search_id:
-            self.send_request(f'find_by_id,{search_id}')
+    help_label = Label(help_window, text=help_text, justify=LEFT)
+    help_label.pack(padx=10, pady=10)
 
-    def find_data_by_name(self):
-        search_name = tk.simpledialog.askstring("Find Data by Name", "Enter Name:")
-        if search_name:
-            self.send_request(f'find_by_name,{search_name}')
+df = pd.read_csv('new_york_housing.csv')
 
-    def find_data_by_filter(self):
-        # Implement similar logic for finding by filter
-        pass
+window = Tk()
+window.title('New York Housing')
+window.config(padx=50, pady=30)
+window.minsize(width=300, height=200)
 
-    def add_data(self):
-        # Implement logic for adding data
-        pass
+title_label = Label(text='New York Housing', width=30)
+title_label.grid(row=0, column=0, columnspan=3)
+space_1 = Label(text='')
+space_1.grid(row=1, column=1)
+space_2 = Label(text='')
+space_2.grid(row=3, column=1)
+space_3 = Label(text='')
+space_3.grid(row=5, column=1)
+space_4 = Label(text='')
+space_4.grid(row=7, column=1)
+space_5 = Label(text='')
+space_5.grid(row=9, column=1)
+space_6 = Label(text='')
+space_6.grid(row=11, column=1)
+space_7 = Label(text='')
+space_7.grid(row=13, column=1)
+space_8 = Label(text='')
+space_8.grid(row=15, column=1)
+space_9 = Label(text='')
+space_9.grid(row=17, column=1)
 
-    def update_data(self):
-        search_id = tk.simpledialog.askstring("Update Data Availability", "Enter ID:")
-        if search_id:
-            new_availability = tk.simpledialog.askinteger("Update Data Availability", "Enter new availability:")
-            if new_availability is not None:
-                self.send_request(f'update,{search_id},{new_availability}')
 
-    def delete_data(self):
-        search_id = tk.simpledialog.askstring("Delete Data", "Enter ID:")
-        if search_id:
-            self.send_request(f'delete,{search_id}')
+show_button = Button(text='Show', command=show)
+show_button.grid(row=2, column=1)
+search_id_button = Button(text='Search by ID', command=search_id)
+search_id_button.grid(row=4, column=1)
+search_name_button = Button(text='Search by Name', command=search_name)
+search_name_button.grid(row=6, column=1)
+search_filter_button = Button(text='Search by Filter', command=search_filter)
+search_filter_button.grid(row=8, column=1)
+add_button = Button(text='Add data', command=add)
+add_button.grid(row=10, column=1)
+update_button = Button(text='Update data', command=update)
+update_button.grid(row=12, column=1)
+delete_button = Button(text='Delete data', command=delete)
+delete_button.grid(row=14, column=1)
+help_button = Button(text='Help', command=help)
+help_button.grid(row=16, column=1)
+exit_button = Button(text='Exit', command=exit)
+exit_button.grid(row=18, column=1)
 
-    def send_request(self, message):
-        # Send request to the server and get the response
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(('localhost', 12345))  # Connect to the server
-        client_socket.send(message.encode())
-        response = client_socket.recv(1024).decode()
-        client_socket.close()
-
-        # Process the response
-        if message == 'show':
-            self.display_data(response)
-        elif message.startswith('find_by_id'):
-            self.display_data(response)
-        # Add similar processing for other actions (find_by_name, find_by_filter, add, update, delete)
-
-    def display_data(self, data):
-        # Display the data using tkinter messagebox or any other GUI component
-        messagebox.showinfo("Result", data)
-
-    def get_data_as_string(self):
-        # Return the entire data as a string (for 'show' action)
-        return df.to_string(index=False)
-
-    def search_by_id(self, search_id):
-        # Implement logic for searching by ID and return the result as a string
-        pass
-
-# Load DataFrame from CSV
-file_path = "new_york_housing.csv"
-df = pd.read_csv(file_path)
-
-# Create and run the GUI
-root = tk.Tk()
-app = HousingApp(root)
-root.mainloop()
+window.mainloop()
