3d82b90e58219b308a3c04dcefa4e3fc3f458883
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-08 04:38:46 +0700

    writing code

---
+++ class/uas/program.py
@@ -0,0 +1,23 @@
+# string1 = '     Halo     '
+# string2 = 'Semua    '
+
+# print(len(string1))
+# print(len(string1.strip()))
+# print(len(string2))
+# print(len(string2.strip()))
+
+# print(string1)
+# print(string1.lstrip())
+# print(len(string1.rstrip().lstrip()))
+# print(len(string1.rstrip()))
+# print(len(string2.rstrip()))
+
+
+# a = 10
+# print(a)
+# a += int('10', 2)
+# print(a)
+# a += int('10', 8)
+# print(a)
+# a += int('0x10', 16)
+# print(a)
