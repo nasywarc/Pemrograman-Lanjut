09cdc75265b930a7bca4b8e39a4c92bd923958dd
Nasywa Azizah <nasywaazizah01@gmail.com>
2023-12-09 20:00:59 +0700

    GUI

--- class/uas/program.py
+++ class/uas/program.py
@@ -23,10 +23,38 @@
 # print(a)
 # print(bin(2))
 
-string = 'pemrograman'
-print(string[-5:])
-print(string[-5:-3])
+# string = 'pemrograman'
+# print(string[-5:])
+# print(string[-5:-3])
 
-file = open('contoh_file.txt', 'a')
-file.write('\nline lima')
-file.close()
+# # cara pertama
+# file = open('contoh_file.txt', 'w')
+# file.write('line satu')
+# file.close()
+
+# # cara kedua (lebih aman)
+# with open('contoh_file.txt', 'a') as file:
+#     file.write('\nline kedua')
+
+from tkinter import *
+
+def click_on_button():
+    global iterate
+    print(iterate)
+    if iterate % 2 == 0:
+        window.config(bg='#000')
+    else:
+        window.config(bg='#FFF')
+    iterate += 1
+    
+iterate = 0
+window = Tk()
+window.title('New Window')
+window.geometry('300x100')
+window.config(padx=100, pady=30)
+
+button1 = Button(text='Click This Button', command=click_on_button, activebackground='#8A383C', activeforeground='#FFF')
+button1.grid(row=0, column=0)
+
+window.mainloop()
+
